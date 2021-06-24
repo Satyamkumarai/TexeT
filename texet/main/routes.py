05ee58dae31929d0a321c3f52c4134028f674f14
Satyamkumarai <satyamkumarai@gmail.com>
2021-06-25 00:09:00 +0530

    Primitive Scanning

--- texet/main/routes.py
+++ texet/main/routes.py
@@ -1,6 +1,21 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for)
 bp = Blueprint('main',__name__)
 @bp.route('/',methods = ['GET'])
 def root():
-    return "Hello!"
+    return render_template("index.html",title="TexeT")
     
+
+@bp.route('/uploadpdf',methods=['GET'])
+def uploadpdf():
+    return "<h1>UPLOAD PDF</h1>"
+    # print(request.form)
+    # return redirect(url_for('main.root'))
+
+@bp.route('/scan',methods=['GET'])
+def scan():
+    return render_template("scan.html",title="TeXeT")
